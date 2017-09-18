63046a6d70986ece82b27c9c817da097acc4ef33
ybinstock <ybinstock@gmail.com>
2017-09-18 14:08:54 +0200

    next

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+function hideInstructions() {
+  document.querySelector('.instructions').setAttribute('visible', 'false');
+}
+setTimeout(hideInstructions, 10000);
 
-  function hideInstructions() {
-    document.querySelector('.instructions').setAttribute('visible', 'false');
-  }
 
-  setTimeout(hideInstructions, 10000);
+function showNext() {
+  document.querySelector('.link').setAttribute('visible', 'true');
+}
+setTimeout(showNext, 10000);
 
